846d3413b6ac1b7527707ec18d389204334431a4
jurewerk <jure.werk@gmail.com>
2018-01-04 09:29:20 +0100

    Update AzureRequest.php

--- src/Crassaert/AzureDocumentDB/Request/AzureRequest.php
+++ src/Crassaert/AzureDocumentDB/Request/AzureRequest.php
@@ -5,68 +5,59 @@
  * @Last Modified by:   cedric
  * @Last Modified time: 2016-01-14 11:45:58
  */
-
 namespace Crassaert\AzureDocumentDB\Request;
-
 class AzureRequest {
-
 	protected $url;
 	protected $key;
 	protected $debug;
-
 	public function __construct($url, $key, $debug = true)
 	{
 		$this->url = $url;
 		$this->key = $key;
 		$this->debug = $debug;
 	}
-
-	public function request($path, $method = 'GET', $options = array(), $resource_type = null, $resource_id = '', $additional_headers = array())
+	public function request($path, $method = 'GET', $options = array(), $resource_type = null, $resource_id = '', $additional_headers = array(), $partition = -1)
 	{
-
 		$curl = curl_init($this->url . '/' . $path);
-
 		//curl_setopt($curl, CURLOPT_HEADER, $this->debug);
 		curl_setopt($curl, CURLOPT_SSLVERSION, 1);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_VERBOSE, $this->debug);
 		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-
 		if (!$resource_type)
 		{
 			$resource_type = current(explode('/', $path));
 		}
-
-		$this->setRequestData($curl, $method, $resource_type, $resource_id, $options, $additional_headers);
-
+		$this->setRequestData($curl, $method, $resource_type, $resource_id, $options, $additional_headers, $partition);
 		($this->debug) ?
 		print "[[Debug: \nReq: ".curl_getinfo($curl, CURLINFO_HEADER_OUT) : $t=1;
-
 		//curl_setopt_array($curl, $options);
 		$result = curl_exec($curl);
 		curl_close($curl);
 		($this->debug) ? print "\nRes: $result]]\n" : $t=1;
-
 		return json_decode($result);
 	}
-
-	protected function setRequestData(&$curl, $method, $resource_type, $resource_id, $options, $additional_headers)
+	protected function setRequestData(&$curl, $method, $resource_type, $resource_id, $options, $additional_headers, $partition)
 	{
+	    if(!is_array($additional_headers)) {
+            $additional_headers = array();
+        }
 		$headers = array_merge($this->getAuthHeaders($method, $resource_type, $resource_id), $additional_headers);
-
 		if ($method == 'POST' || $method == 'PUT')
 		{                                        
 			$data = json_encode($options);
-
 			if (isset($options['query']))
 			{
 				$headers[] = 'x-ms-documentdb-isquery: True';
 				// Added for crosspartiton
-				$headers[] = 'x-ms-documentdb-query-enablecrosspartition: true';
+				$headers[] = 'x-ms-documentdb-query-enablecrosspartition: True';
+               // $my = array(1,2);
+               // $my = json_encode($my);
+                //$headers[] = 'x-ms-documentdb-partitionkeyrangeid: "0"';
+                //$headers[] = 'x-ms-documentdb-partitionkey: ["V-46777"]';
                 $headers[] = 'x-ms-max-item-count: -1';
 				$headers[] = 'Content-Type: application/sql';
 				$headers[] = 'Content-Length: ' . strlen($options['query']);
-
 				curl_setopt($curl, CURLOPT_POSTFIELDS, $options['query']);
 			}
 			else
@@ -76,16 +67,15 @@ class AzureRequest {
 				$headers[] = 'x-ms-documentdb-query-enablecrosspartition: true';
                 $headers[] = 'x-ms-max-item-count: -1';
 				$headers[] = 'Content-Length: ' . strlen($data);
-
 				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 			}
 		}
-
+		if($partition > -1) {
+            $headers[] = 'x-ms-documentdb-partitionkeyrangeid: ' . $partition;
+        }
 		$headers[] = 'api-key:' . $this->key;
 		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);  
 	}
-
-
 	/**
    * getAuthHeaders
    *
@@ -101,16 +91,13 @@ class AzureRequest {
 		$x_ms_date = gmdate('D, d M Y H:i:s T', strtotime('+2 minutes'));
 		$master = 'master';
 		$token = '1.0';
-
 		$key = base64_decode($this->key);
 		$string_to_sign = $verb . "\n" .
 		$resource_type . "\n" .
 		$resource_id . "\n" .
 		$x_ms_date . "\n" .
 		"\n";
-
 		$sig = base64_encode(hash_hmac('sha256', strtolower($string_to_sign), $key, true));
-
 		return Array(
 			'Accept: application/json',
 			'User-Agent: documentdb.php.sdk/1.0.0',
